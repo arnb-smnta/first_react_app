6a737e8073d35f13176377767489860d1812c4a2
loveumearnab2812000 <loveumearnab.2812000@gmail.com>
2023-05-20 23:15:12 +0530

    added expenses monthly dynamic graph

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,3 +1,23 @@
 import ChartBar from "./ChartBar";
-const Chart = () => {};
+import "./Chart.css";
+const Chart = (props) => {
+  const dataPointValue = props.dataPoints.map((dataPoint) => {
+    return dataPoint.value;
+  });
+  const MaxExpense = Math.max(...dataPointValue);
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => {
+        return (
+          <ChartBar
+            key={dataPoint.label}
+            value={dataPoint.value}
+            maxValue={MaxExpense}
+            label={dataPoint.label}
+          />
+        );
+      })}
+    </div>
+  );
+};
 export default Chart;
